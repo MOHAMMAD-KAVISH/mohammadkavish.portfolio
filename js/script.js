7e9a612fb0727b6a5420ba21fb9aaa926a22cd1d
MOHAMMAD KAVISH <119128166+MOHAMMAD-KAVISH@users.noreply.github.com>
2024-08-29 23:28:56 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,20 +6,3 @@ var typed=new Typed(".typing",{
 })
 
 
-// Wait for the DOM to load
-document.addEventListener("DOMContentLoaded", function() {
-    // Select all nav links
-    const navLinks = document.querySelectorAll('.nav a');
-
-    // Add click event listener to each nav link
-    navLinks.forEach(link => {
-        link.addEventListener('click', function() {
-            // Remove 'active' class from all links
-            navLinks.forEach(nav => nav.classList.remove('active'));
-
-            // Add 'active' class to the clicked link
-            this.classList.add('active');
-        });
-    });
-});
-
